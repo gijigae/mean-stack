1a64e69cba33819bda53f091412afa4e73e9f056
Sangmin Ahn <gijigae@gmail.com>
2016-02-19 17:15:09 +0900

    Add mongodb connection

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -3,5 +3,13 @@
  */
 /* GET home page */
 module.exports.about = function(req, res, next){
-  res.render('generic-text', {title: 'About'})
+  res.render('generic-text', {
+    title: 'About Loc8r',
+    content: 'Loc8r was created to help people find places to sit down and get a bit of work done. \n\n' +
+    'Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur arcu erat, ' +
+    'accumsan id imperdiet et, porttitor at sem.Nulla quis lorem ut libero malesuada feugiat. Curabitur ' +
+    'aliquet quam id dui posuere blandit. Quisque velit nisi, pretium ut lacinia in, elementum id enim. ' +
+    'Sed porttitor lectus nibh. Curabitur aliquet quam id dui posuere blandit. Nulla porttitor accumsan tincidunt. ' +
+    'Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Sed porttitor lectus nibh.'
+  })
 }
---
+++ app_server/models/db.js
@@ -0,0 +1,53 @@
+/**
+ * Created by gijigae on 2/19/16.
+ */
+var mongoose = require('mongoose');
+var gracefulShutdown;
+var dbURI = 'mongodb://localhost/Loc8r';
+if(process.env.NODE_ENV === 'production'){
+
+  dbURI = process.env.MONGOLAB_URI;
+  console.log('NODE_ENV:' + process.env.NODE_ENV + ', dbURI: ' + dbURI);
+}
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function(){
+  console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function(err){
+  console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function(){
+  console.log('Mongoose disconnected');
+});
+
+gracefulShutdown = function(msg, callback){
+  mongoose.connection.close(function(){
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+};
+
+// For nodemon restarts
+process.once('SIGUSR2', function(){
+  gracefulShutdown('nodemon restart', function(){
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+
+// For app termination
+process.once('SIGINT', function(){
+  gracefulShutdown('app termination', function(){
+    process.exit(0);
+  });
+});
+
+// For Heroku app termination
+process.once('SIGTERM', function(){
+  gracefulShutdown('Heroku app shutdown', function(){
+    process.exit(0);
+  });
+});
+
+require('./locations');
